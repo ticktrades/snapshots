6deeace053b046be729467cfda7b3d4705c2fe8c
venky <venkatesh.sridharan@comeon.com>
2020-01-29 23:25:53 +0100

    Few updates

--- src/footer.svelte
+++ src/footer.svelte
@@ -32,35 +32,41 @@
 		background-image: url('/f_logo_RGB-Blue_100.png');
 	}
 	.meta{
-		text-align: center;
+		text-align: right;
+		font-size: 0.6rem;
 	}
 	.link-ticktrades {
 		background-image: url('/android-chrome-512x512.png');
 		display: inline-block;
 		margin: 0;
 		padding-left: 2rem;
+		font-size: 0.6rem;
 	}
 </style>
 
 <footer>
 	<section class="social-media">
 		<div class="contact-link contact-twitter">
-			Follow us on twitter
+			Follow 
 			<a href="https://www.twitter.com/ticktrades_com">@ticktrades_com</a>
 		</div>
 		<div class="contact-link contact-facebook">
-			Like us on facebook
+			Like
 			<a href="https://www.facebook.com/ticktrades">
 				facebook.com/ticktrades
 			</a>
 		</div>
 	</section>
 	<div class="meta">
-		Report generated and on {date} &copy; by
+		Report by
 		<a
 			class="contact-link link-ticktrades"
 			href="https://www.ticktrades.com">
-			https://www.ticktrades.com
+			Ticktrades
 		</a>
+	
+	</div>
+	<div class="meta">
+		Generated on {date}
 	</div>
 </footer>
