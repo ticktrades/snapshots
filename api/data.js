809279e351044a6f1009112f83cd7decba3b9cc5
Dilip Kr. Shukla <contact@fusionstrings.com>
2020-02-05 13:53:46 +0100

    api update

--- api/data.js
+++ api/data.js
@@ -1,27 +1,55 @@
 const got = require('got');
 
+function getIntlNum(number) {
+    const intlFormattingOption = {
+        notation: 'compact',
+        compactDisplay: 'long',
+        currency: 'USD',
+        currencyDisplay: 'code',
+        unitDisplay: 'long'
+    };
+    const locale = 'en';
+    return new Intl.NumberFormat(locale, intlFormattingOption).format(
+        Number.parseFloat(number)
+    );
+}
+
 async function getTradeData() {
     try {
-        const response = await got('https://api.coingecko.com/api/v3/global');
+        const { body } = await got('https://api.coingecko.com/api/v3/global');
         //console.log(body);
-        const json =response.body; //await response.json();
-        console.log(typeof json)
-        const { market_cap_percentage, total_market_cap } = JSON.parse(json).data;
+        const json = JSON.parse(body); //await response.json();
+
+        const { market_cap_percentage, total_market_cap } = json.data;
         // console.log(total_market_cap);
         const sum_total_market_cap = Object.values(total_market_cap).reduce(
             (previous, current) =>
-                Number.parseFloat(previous) + Number.parseFloat(current),
-            0
+                Number.parseFloat(previous) + Number.parseFloat(current)
         );
 
+        const market_cap_percentage_btc = market_cap_percentage.btc;
+        const market_cap_percentage_btc_label = Number.parseFloat(
+            market_cap_percentage_btc
+        ).toFixed(2);
+
+        const market_cap_percentage_rest_label = (
+            100 - Number.parseFloat(market_cap_percentage_btc)
+        ).toFixed(2);
+
+        const market_cap_btc_intl = getIntlNum(
+            (total_market_cap.usd * market_cap_percentage_btc) / 100
+        );
+
+		const market_cap_alt_intl = getIntlNum(total_market_cap.usd - ((total_market_cap.usd * market_cap_percentage_btc) / 100))
         return {
-            market_cap_percentage,
-            sum_total_market_cap,
-            market_cap_btc: total_market_cap.btc,
-            market_cap_usd: total_market_cap.usd
+            market_cap_percentage_btc_label,
+            market_cap_percentage_rest_label,
+            market_cap_btc_intl: `USD ${market_cap_btc_intl}`,
+            market_cap_alt_intl: `USD ${market_cap_alt_intl}`
         };
     } catch (error) {
         console.log(error);
+        return new Error(error);
         //=> 'Internal server error ...'
     }
 }
